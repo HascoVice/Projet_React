180c2769c779746ceee5766c7adab71d3b2e1efe
Hassan Aidibe <101555873+HascoVice@users.noreply.github.com>
2022-06-10 15:04:24 +0200

    new

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <div className="allArticle">
+      <div className="Article">
+        <div className="Article__details">
+          <h1>Title 1</h1>
+          <p>01.05.2022</p>
+        </div>
+        <div className="Article__picture">
+          <img src="https://placewaifu.com/image/500/272" />
+          <p>niah niah niouh ninano</p>
+        </div>
+        <p className="Description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, neque
+          qui quaerat sed, architecto consectetur facilis sunt molestias eveniet
+          suscipit eius omnis illo rem. Et facilis eum minus. Ullam, amet?
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="Article">
+        <div className="Article__details">
+          <h1>Title 1</h1>
+          <p>01.05.2022</p>
+        </div>
+        <div className="Article__picture">
+          <img src="https://placewaifu.com/image/500/272" />
+          <p>niah niah niouh ninano</p>
+        </div>
+        <p className="Description">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse officiis
+          nihil aspernatur eius suscipit necessitatibus, dolore magni dicta
+          harum sed sunt unde, non recusandae rem iste nobis laboriosam quas
+          ullam!
+        </p>
+      </div>
+      <div className="Article">
+        <div className="Article__details">
+          <h1>Title 1</h1>
+          <p>01.05.2022</p>
+        </div>
+        <div className="Article__picture">
+          <img src="https://placewaifu.com/image/500/272" />
+          <p>niah niah niouh ninano</p>
+        </div>
+        <p className="Description">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum
+          recusandae architecto veritatis, sapiente iste repellendus dolore
+          doloremque, harum tempora perspiciatis
+        </p>
+      </div>
     </div>
   );
 }
