499c7b67d9972860d319d6a820dd1d487d7b79ef
Jessica Marie Sainte <J_mariesainte1@hetic.eu>
2022-06-27 00:34:33 +0200

    commit last commit

--- src/components/menu-filter.js
+++ src/components/menu-filter.js
@@ -3,107 +3,116 @@ import React from "react";
 import "../components/menu-filter.css";
 import classNames from "classnames";
 let categorie;
-class Sidebar extends React.Component{
-
-  constructor(props){
-    super(props)
-    this.Input = this.Input.bind(this)
+class Sidebar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.Input = this.Input.bind(this);
   }
 
-  Input(event){
-this.props.input(event.target.value)
-console.log(event.target.value)
+  Input(event) {
+    this.props.input(event.target.value);
+    console.log(event.target.value);
   }
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="menu-filter">
-<p>Trier par:</p>
-<div className="Keyword">
-  <div className="form__grouped field">
-    <input
-      type="text"
-      className="form__fielded"
-      placeholder="Name"
-      name="Keyword"
-      id="Keyword"
-      required=""
-    />
-    <label className="labelmenu form__label" htmlFor="Keyword">
-      Mot-Clé
-    </label>
-  </div>
-</div>
-<ul className="ul__menu">
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="categorie" onChange={this.Input} value="figurine" />
-      <label className="labelmenu" htmlFor="c1">Figurines</label> 
-    </div>
-  </li>
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="categorie" onChange={this.Input} value="vetement" />
-      <label className="labelmenu" htmlFor="c1">Vetements</label>
-    </div>
-  </li>
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="categorie" onChange={this.Input} value="deco" />
-      <label className="labelmenu" htmlFor="c1">Decoration</label>
-    </div>
-  </li>
-  <div className="Price">
-    <div className="form__grouped field">
-      <input
-        type="number"
-        className="form__fielded"
-        placeholder="Name"
-        name="firstprice"
-        id="firstprice"
-        required=""
-      />
-      <label className="labelmenu form__label" htmlFor="firstprice" >
-        Price
-      </label>
-    </div>
-    <span className="A"> À </span>
-    <div className="form__grouped field">
-      <input
-        type="number"
-        className="form__fielded"
-        placeholder="Name"
-        name="secondprice"
-        id="secondprice"
-        required=""
-      />
-      <label className="labelmenu form__label" htmlFor="secondprice" >
-        Price
-      </label>
-    </div>
-  </div>
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="category" onChange={this.Input} value="note" />
-      <label className="labelmenu" htmlFor="c1">Les mieux notés</label>
-    </div>
-  </li>
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="category" onChange={this.Input} value="price" />
-      <label className="labelmenu" htmlFor="c1">Bon plan</label>
-    </div>
-  </li>
-  <li className="ulli">
-    <div className="div">
-      <input className="inputChecking" id="c1" type="radio" name="category" onChange={this.Input} value="stock" />
-      <label className="labelmenu" htmlFor="c1">Stock épuisé</label>
-    </div>
-  </li>
-</ul>
-</div>
-  );
+        <p className="entete">Trier par:</p>
+
+        <ul className="ul__menu">
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="a1"
+                type="radio"
+                name="categorie"
+                onChange={this.Input}
+                value="figurine"
+              />
+              <label className="labelmenu" htmlFor="a1">
+                Figurines
+              </label>
+            </div>
+          </li>
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="b1"
+                type="radio"
+                name="categorie"
+                onChange={this.Input}
+                value="vetement"
+              />
+              <label className="labelmenu" htmlFor="b1">
+                Vetements
+              </label>
+            </div>
+          </li>
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="c1"
+                type="radio"
+                name="categorie"
+                onChange={this.Input}
+                value="deco"
+              />
+              <label className="labelmenu" htmlFor="c1">
+                Decoration
+              </label>
+            </div>
+          </li>
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="d1"
+                type="radio"
+                name="category"
+                onChange={this.Input}
+                value="note"
+              />
+              <label className="labelmenu" htmlFor="d1">
+                Les mieux notés
+              </label>
+            </div>
+          </li>
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="e1"
+                type="radio"
+                name="category"
+                onChange={this.Input}
+                value="price"
+              />
+              <label className="labelmenu" htmlFor="e1">
+                Bon plan
+              </label>
+            </div>
+          </li>
+          <li className="ulli">
+            <div className="div">
+              <input
+                className="inputChecking"
+                id="f1"
+                type="radio"
+                name="category"
+                onChange={this.Input}
+                value="stock"
+              />
+              <label className="labelmenu" htmlFor="f1">
+                Stock épuisé
+              </label>
+            </div>
+          </li>
+        </ul>
+      </div>
+    );
   }
-    
 }
-export default Sidebar
+export default Sidebar;
